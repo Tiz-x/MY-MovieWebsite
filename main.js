8f0f138a6e4db35dfc284e8efe7b7ccd1d7884b9
OPAWALE TIMILEYIN <opawaletimi@gmail.com>
2025-04-18 01:20:07 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,39 @@
+
+
+// document.addEventListener('DOMContentLoaded', function() {
+//   const viewMoreBtn = document.getElementById('view-more-btn');
+//   const hiddenMovies = document.querySelector('.hidden-movies');
+  
+//   if (viewMoreBtn && hiddenMovies) {
+//     console.log("Button and hidden movies found");
+//     viewMoreBtn.addEventListener('click', function() {
+//       console.log("Button clicked");
+      
+//       if (hiddenMovies.style.display === 'none' || hiddenMovies.style.display === '') {
+//         hiddenMovies.style.display = 'grid';
+//         viewMoreBtn.textContent = 'Show Less';
+//         console.log("Showing more movies");
+//       } else {
+//         hiddenMovies.style.display = 'none';
+//         viewMoreBtn.textContent = 'View All Coming Soon';
+//         console.log("Hiding movies");
+//       }
+//     });
+//   } else {
+//     console.log("Button or hidden movies not found");
+//   }
+  
+//   var homeSwiper = new Swiper(".home", {
+//     spaceBetween: 30,
+//     centeredSlides: true,
+//     autoplay: {
+//       delay: 3500,
+//       disableOnInteraction: false,
+//     },
+//     pagination: {
+//       el: ".swiper-pagination",
+//       clickable: true,
+//     },
+//   });
+// });
+
